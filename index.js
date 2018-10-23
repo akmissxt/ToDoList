24a97de3eb222d17c4355f9b9fe7abb47ecf0ae9
Ivan Leung <ivanlienmi@hotmail.com>
2018-10-23 16:11:30 +0800

    implement basic function

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 'use strict'
 
 const express = require('express');
-
 const app = express();
-
-let toDoController = require('./controller/toDoController')(app);
+const bodyParser = require('body-parser');
 
 //模板引擎
 app.set('views', 'public/pages');
 app.set('view engine', 'ejs');
+
 //静态资源
 app.use(express.static('./public'));
+
+//请求体解析
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Index')
 })
+
+let toDoController = require('./controller/toDoController')(app);
 
 app.listen(8888, () => {
     console.log('listening port of 8888');
